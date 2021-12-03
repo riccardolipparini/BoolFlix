7d779fe14e3d7721840885122ac82d161ca15400
Riccardo <riccardolipparini0@gmail.com>
2021-12-03 15:54:38 +0100

    added flags

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -5,17 +5,19 @@
             <h1>{{films.title}}</h1>
             <h2>{{films.original_title}}</h2>
             <h3>{{films.vote_average}}</h3>
-            <h4>{{films.original_language}}</h4>
+            <div class="flag">
+                <img v-if="getFlags(films) !== `notfound`" :src="getFlags(films)" alt="">
+                <p v-if="films.original_language === `notfound`">{{films.original_language}}</p>
+            </div>
           </div>
       
-
-       
-          <div class="cards" v-for="series, s in detailsSeries" :key="s">
+          <div class="cards" v-for="series, i  in detailsSeries" :key="i">
             <h1>{{series.name}}</h1>
             <h2>{{series.original_title}}</h2>
             <h3>{{series.vote_average}}</h3>
             <div class="flag">
-                <img v-if="getFlags() !== `notfound`" :src="getFlags(series)" alt="">
+                <img v-if="getFlags(series) !== `notfound`" :src="getFlags(series)" alt="">
+                <p v-if="series.original_language === `notfound`">{{series.original_language}}</p>
             </div>
           </div>
        </div>
@@ -38,8 +40,10 @@ methods:{
             return `https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/280px-Flag_of_the_United_Kingdom_%283-5%29.svg.png`
         }else if(flag.original_language === `it`){
             return `https://upload.wikimedia.org/wikipedia/commons/c/ca/Bandiera_italiana_foto.svg`
-        }else if(flag.original_language === ``){
-            return `https://upload.wikimedia.org/wikipedia/commons/c/ca/Bandiera_italiana_foto.svg`
+        }else if(flag.original_language === `ja`){
+            return `https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/280px-Flag_of_Japan.svg.png`
+        }else{
+            return `notfound`
         }
     }
         
